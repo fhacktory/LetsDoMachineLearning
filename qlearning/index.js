fbc33192e94fc83bc7685aae0361e191cee627db
Ton Nom <ton@email.tld>
2015-03-29 11:01:09 +0000

    RightCommit

--- qlearning/index.js
+++ qlearning/index.js
@@ -7,7 +7,7 @@ var IaPlayer = require('./iaplayer.js');
 var iterations = 10000;
 
 var qplayer = new QPlayer(0, 0.9).loadFromFile("trained.qp");
-// var dumbplayer = new DumbPlayer(1);
+// var Iaplayer = new DumbPlayer(1);
 var Iaplayer = new IaPlayer(1);
 
 process.on('SIGINT', function() {
