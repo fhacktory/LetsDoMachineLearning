c89692701603b5af1a34760d3a943621252b1fd6
Geekuillaume <geek.uillaume@gmail.com>
2015-03-29 07:44:27 +0200

    Handling the case that the qplayer is the number 1

--- qlearning/qplayer.js
+++ qlearning/qplayer.js
@@ -8,56 +8,67 @@ var QPlayer = function(playerId, G) {
     this.playerId = playerId;
     this.lastStateSerialization = null;
     this.turns = 0;
+    this.learningActivated = true;
     this.G = G || 0.9;
 };
 
-QPlayer.prototype.getMaxProba = function(freeMoves, state) {
-    var probaMax = -Infinity;
-    var posMax = [];
+QPlayer.prototype.getProba = function(freeMoves, state) {
+    var probaExtreme = this.playerId === 0 ? -Infinity : Infinity;
+    var posExtreme = [];
     for (var i = freeMoves.length - 1; i >= 0; i--) {
         var proba = this.getProbaFromKey(state.createFromMove(freeMoves[i], this.playerId).serialize());
-        if (proba > probaMax) {
-            probaMax = proba;
-            posMax = [freeMoves[i]];
-        } else if (proba === probaMax) {
-            posMax.push(freeMoves[i]);
+        if ((this.playerId === 0) ? (proba > probaExtreme) : (proba < probaExtreme)) {
+            probaExtreme = proba;
+            posExtreme = [freeMoves[i]];
+        } else if (proba === probaExtreme) {
+            posExtreme.push(freeMoves[i]);
         }
     };
-    return posMax;
+    return posExtreme;
 }
 
 QPlayer.prototype.getProbaFromKey = function(key) {
     return this.scores[key] || 0;
 }
 
+QPlayer.prototype.setLearningState = function(learningState) {
+    this.learningActivated = learningState;
+    return this;
+}
+
 QPlayer.prototype.getTurn = function(state) {
     var i = Math.random();
     var move;
-    if (i <= 0.1) {
+    if (i <= 0.1 && this.learningActivated) {
         move = state.getFreeMove();
     } else {
         var freeMoves = state.getFreeMoves();
-        var resultProbas = this.getMaxProba(freeMoves, state);
-        if (resultProbas.length == 1) {
+        var resultProbas = this.getProba(freeMoves, state);
+        if (resultProbas.length === 1) {
             move = resultProbas[0];
         } else {
             move = resultProbas[Math.floor(Math.random() * resultProbas.length)];
         }
     }
-    var newState = state.createFromMove(move, this.playerId);
-    var newStateSerialization = newState.serialize();
-    if (newState.getWinner() == this.playerId)
-        this.scores[newStateSerialization] = 1;
-    var serialization = state.serialize();
-    this.counts[serialization] = this.counts[serialization] ? this.counts[serialization] + 1 : 1;
-    this.scores[serialization] = (this.scores[serialization] || 0) + (1 / (1 + this.counts[serialization])) * (this.G * (this.scores[newStateSerialization] || 0) - (this.scores[serialization] || 0));
-    this.lastStateSerialization = newStateSerialization;
-    this.turns++;
+    if (this.learningActivated) {
+        var newState = state.createFromMove(move, this.playerId);
+        var newStateSerialization = newState.serialize();
+        if (newState.getWinner() === this.playerId) {
+            this.scores[newStateSerialization] = 1;
+        }
+        var serialization = state.serialize();
+        this.counts[serialization] = this.counts[serialization] ? this.counts[serialization] + 1 : 1;
+        this.scores[serialization] = (this.scores[serialization] || 0) + (1 / (1 + this.counts[serialization])) * (this.G * (this.scores[newStateSerialization] || 0) - (this.scores[serialization] || 0));
+        this.lastStateSerialization = newStateSerialization;
+        this.turns++;
+    }
     return move;
 }
 
 QPlayer.prototype.punish = function(ratio) {
-    this.scores[this.lastStateSerialization] = ratio;
+    if (this.learningActivated) {
+        this.scores[this.lastStateSerialization] = ratio;
+    }
     return this;
 }
 
